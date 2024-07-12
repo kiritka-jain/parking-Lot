1c47b0b970f755569fb2b77fd2f73aa3d100eb67
Kirtika Jain <jain.kirtika08@gmail.com>
2024-07-12 16:10:11 +0530

    add car controllers and service.

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,28 +1,74 @@
-// Example car controller
+
+import { CarService } from '../services/carService.js';
+import { NotFoundError,ValidationError } from '../errors/customErrors.js';
+
 
 const carController = {
   getCarInfo: (req, res) => {
-    res.send('Get all car information');
+    res.render("car", {name: "test"});
   },
 
-  addCar: (req, res) => {
-    res.send('Add a new car');
+  addCar: async(req, res) => {
+    const { make, model, color, number, year } = req.body;
+  
+    try {
+      const newCar = await CarService.addCar({ make, model, color, number, year });
+      res.status(201).json(newCar);
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        res.status(400).json({ message: error.message });
+      } else {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+      }
+    }
   },
 
-  getCarById: (req, res) => {
+  getCarById: async(req, res) => {
     const carId = req.params.id;
-    res.send(`Get car with ID: ${carId}`);
+    try {
+      const carId = req.params.id;
+      const car = await CarService.getCarById(carId);
+
+      if (!car) {
+         return res.status(404).send('Car not found');
+      }
+
+      res.render('car_details', {
+         make: car.make,
+         model: car.model,
+         color: car.color,
+         number: car.number,
+         year: car.year
+      });
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        res.status(error.statusCode).json({ message: error.message });
+      } else {
+        console.error(error);
+        res.status(error.statusCode || 500).json({ message: error.message || 'Server error' });
+      }
+    }
   },
 
-  updateCar: (req, res) => {
+  deleteCar: async(req, res) => {
     const carId = req.params.id;
-    res.send(`Update car with ID: ${carId}`);
-  },
+    try {
+      const message = await CarService.deleteCarById(carId);
+      res.status(200).json({ message: message });
 
-  deleteCar: (req, res) => {
-    const carId = req.params.id;
-    res.send(`Delete car with ID: ${carId}`);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        res.status(error.statusCode).json({ message: error.message });
+      } else {
+        console.error(error);
+        res.status(error.statusCode || 500).json({ message: error.message || 'Server error' });
+      }
+    }
+  },
+  updateCar: (req,res)=>{
+    console.log("Car updated Sucessfully.")
   }
-};
+}
 
 export default carController;
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import indexRouter from './routes/index.js';
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
 
-app.use('/api', indexRouter);
+// Set the views directory
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-  res.send('Home Page');
-});
+app.use(bodyParser.json());
+
+app.use('/', indexRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
